c0e72aa5a7b0a2135fe42504b562d2b1022d5cab
Christopher Danielson <christopher.a.danielson@protonmail.com>
2023-07-27 19:20:12 -0400

    Finish courseinfo step 7

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,12 +1,20 @@
 import Part from './Part'
+import Total from './Total'
 
 const Content = ({ parts }) => {
+  const initialValue = 0
+  const total = parts.map(part => part.exercises)
+    .reduce((total, current) => total + current, initialValue)
+
+  console.log(total)
 
   return (
     <>
       {parts.map(part =>
         <Part key={part.id} part={part} />
       )}
+      
+      <Total total={total} />
     </>
   )
 }
